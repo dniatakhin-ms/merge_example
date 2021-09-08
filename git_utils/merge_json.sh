05c12328368bfc14f07eac283ce876ca1bf9969f
Denis Netahin <d.netahin@melsoft-games.com>
2021-09-08 22:36:46 +0300

    --

--- git_utils/merge_json.sh
+++ git_utils/merge_json.sh
@@ -5,6 +5,7 @@ case "$OSTYPE" in
     echo "OSX"
     $(pwd)/git_utils/osx/./MergeUtil $1 $2 $3 $4 ;;
   msys*)    
+  echo "merge $1"
 	MergeUtil.exe $1 $2 $3 $4 ;;
   *)        echo "unknown: $OSTYPE" ;;
 esac
